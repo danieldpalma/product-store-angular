cf801b2e92c1f4a06d122585768fda4b84384151
danieldpalma <danielpalma232@gmail.com>
2025-04-01 11:17:48 -0300

    Projeto criado, angular material adicionado  e salvando produto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ListComponent } from './features/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent,
+  },
+  {
+    path: 'create-product',
+    loadComponent: () =>
+      import('./features/create/create.component').then(
+        (m) => m.CreateComponent
+      ),
+  },
+];
---
+++ src/app/features/create/create.component.ts
@@ -0,0 +1,44 @@
+import { Component, inject } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ProductsService } from '../../shared/services/products.service';
+
+@Component({
+  selector: 'app-create',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+  ],
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.scss',
+})
+export class CreateComponent {
+  productService = inject(ProductsService);
+
+  form = new FormGroup({
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  });
+
+  onSubmit() {
+    this.productService
+      .post({
+        title: this.form.controls.title.value,
+      })
+      .subscribe(() => {
+        alert('Sucesso!');
+      });
+  }
+}
