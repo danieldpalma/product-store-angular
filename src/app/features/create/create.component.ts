d0fb75c0246ea65e850b26fe603f027af6d909c3
danieldpalma <danielpalma232@gmail.com>
2025-04-03 12:27:05 -0300

    refatorado formulario para edição e criação

--- src/app/features/create/create.component.ts
+++ src/app/features/create/create.component.ts
@@ -1,51 +1,26 @@
 import { Component, inject } from '@angular/core';
-import {
-  FormControl,
-  FormGroup,
-  ReactiveFormsModule,
-  Validators,
-} from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { ProductsService } from '../../shared/services/products.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { FormComponent } from '../../shared/components/form/form.component';
+import { Product } from '../../shared/interfaces/product.interface';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [
-    ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatButtonModule,
-  ],
+  imports: [FormComponent],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss',
 })
 export class CreateComponent {
   productService = inject(ProductsService);
-
   matSnackBar = inject(MatSnackBar);
-
   router = inject(Router);
 
-  form = new FormGroup({
-    title: new FormControl<string>('', {
-      nonNullable: true,
-      validators: Validators.required,
-    }),
-  });
-
-  onSubmit() {
-    this.productService
-      .post({
-        title: this.form.controls.title.value,
-      })
-      .subscribe(() => {
-        this.matSnackBar.open('Produto salvo com sucesso!', 'Ok');
-        this.router.navigateByUrl('/');
-      });
+  onSubmit(product: Product) {
+    this.productService.post(product).subscribe(() => {
+      this.matSnackBar.open('Produto criado com sucesso!', 'Ok');
+      this.router.navigateByUrl('/');
+    });
   }
 }
