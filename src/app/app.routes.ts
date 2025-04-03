f5567bdcddaa20aac5161d62d9c0a3abac338db1
danieldpalma <danielpalma232@gmail.com>
2025-04-03 11:47:25 -0300

    adicionado funcionalidade de editar produto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,13 @@
-import { Routes } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Routes,
+} from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { Observable } from 'rxjs';
+import { Product } from './shared/interfaces/product.interface';
+import { inject } from '@angular/core';
+import { ProductsService } from './shared/services/products.service';
 
 export const routes: Routes = [
   {
@@ -14,7 +22,13 @@ export const routes: Routes = [
       ),
   },
   {
-    path: 'edit-product',
+    path: 'edit-product/:id',
+    resolve: {
+      product: (route: ActivatedRouteSnapshot) => {
+        const productsService = inject(ProductsService);
+        return productsService.getById(route.paramMap.get('id') as string);
+      },
+    },
     loadComponent: () =>
       import('./features/edit/edit.component').then((m) => m.EditComponent),
   },
